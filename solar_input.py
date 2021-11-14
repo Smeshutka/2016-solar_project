d1a4e9dc449c149975b7aee1c62f1869acf630da
Vladislav <kharavinin.va@phystech.edu>
2021-11-14 12:45:54 +0300

    сделали запись в файл

--- solar_input.py
+++ solar_input.py
@@ -97,7 +97,13 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            #print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            if obj.type == "star":
+                s = "Star " +str(obj.R) +' '+ str(obj.color)+' '+str(obj.m)+' '+str(obj.x)+' '+str(obj.y)+' '+str(obj.Vx)+' '+str(obj.Vy)
+                out_file.write(s+'\n')
+            if obj.type == "planet":
+                s = "Planet "+str(obj.R) +' '+str(obj.color)+' '+str(obj.m)+' '+str(obj.x)+' '+str(obj.y)+' '+str(obj.Vx)+' '+str(obj.Vy)
+                out_file.write(s+'\n')
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
